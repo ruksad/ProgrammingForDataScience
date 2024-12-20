f244dabfc95ea9d11d2f2c9d1f0182048579f51f
ruksad <sidsid2810@gmail.com>
2024-12-20 10:37:33 +0530

    adding main to file

--- SamplesMatchingMarbles_02.py
+++ SamplesMatchingMarbles_02.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-sample_size=10000
-urn=["b","b","b","w","w"]
 
-sample=[np.random.choice(urn, size=2, replace=False) for i in range(sample_size)]
-is_matching= [ marble1==marble2 for marble1, marble2 in sample]
-print(f"Portion of samples with matching marbles: {np.mean(is_matching)}")
+def portion_of_sample():
+    sample_size = 10000
+    urn = ["b", "b", "b", "w", "w"]
+
+    sample = [np.random.choice(urn, size=2, replace=False) for i in range(sample_size)]
+    is_matching = [marble1 == marble2 for marble1, marble2 in sample]
+    print(f"Portion of samples with matching marbles: {np.mean(is_matching)}")
+
+
+if __name__ == "__main__":
+    portion_of_sample()
