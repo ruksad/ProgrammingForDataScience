bffb4e6a9d0b8a91e6861a963bdd06f96d681718
ruksad <sidsid2810@gmail.com>
2024-12-30 18:10:57 +0530

    checking file size utility

--- 10_check_file_encoding.py
+++ 10_check_file_encoding.py
@@ -1,6 +1,9 @@
 from pathlib import Path
-
 import chardet
+import os
+
+import numpy as np
+
 from timeit_util import time_decorator
 
 
@@ -10,9 +13,24 @@ def detect_file_encoding():
     print(line('File name', 'Encoding', 'Confidence'))
 
     for filepath in Path('sources').glob('*'):
-        detect = chardet.detect(filepath.read_bytes())  # tells you encoding of the file data and gives the confidence too
+        detect = chardet.detect(filepath.read_bytes())  # tells you encoding of the file data and gives the
+        # confidence too, we need to specify this encoding while reading into pandas
+        # pd.read_csv('data/businesses.csv', encoding='ISO-8859-1')
         print(line(str(filepath), detect['encoding'], detect['confidence']))
+
+
+@time_decorator
+def check_file_size(dir):
+    KiB = 1024
+    MiB= 1024**2
+    line = '{:<35}{:<25}{}'.format
+    print(line('File name', 'File size in KiB', 'File size in MiB'))
+
+    for filepath in Path(dir).glob('*'):
+        getsize = os.path.getsize(filepath) # number of chars
+        print(line(str(filepath), np.round(getsize/KiB), np.round(getsize/MiB)))
 
 
 if __name__ == "__main__":
     detect_file_encoding()
+    check_file_size('sources')
