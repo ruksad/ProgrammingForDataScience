3b9a5cd448df9d9d0183399513467cbb182201d5
ruksad <sidsid2810@gmail.com>
2025-01-12 16:20:27 +0530

    learning plotly

--- 11_1_plotly_learn.py
+++ 11_1_plotly_learn.py
@@ -34,10 +34,29 @@ def plot_weight_longevity():
     fig.update_layout(margin= dict(t=30))
     fig.show()
 
+def plot_year_price():
+    groupBydogs= dogs.groupby("size")["purchase_price"].mean().reset_index()
+    line= px.line(groupBydogs,x="size",y="purchase_price", width=450, height=350)
+    line.show()
+
+def plot_historgram():
+    size_lifespan= dogs.groupby('size')['longevity'].mean().reset_index()
+    histo= px.histogram(size_lifespan, x="size", y="longevity",width= 650, height= 550)
+    histo.show()
+
+def scatter_with_plotting_symbol():
+    scatter= px.scatter(dogs, x="height",y="weight",
+               color="size", symbol="size",
+               labels=dict(height="Height in CM", weight="Weight in Kgs", size="Sizezz"),
+               width= 650, height=550)
+    scatter.add_annotation(text= "76,79 is right most point", x=76,y=79, ax=50, ay=15, xshift=3, xanchor='center')
+    scatter.show()
 
 if __name__ == "__main__":
     # sfh_df = read_csv_from(Path('sources'))
     #sfh_df.info()
     dogs = read_data_dogs_source(Path('sources'))
     #create_plot_with_facet_on_size()
-    plot_weight_longevity()
+    #plot_weight_longevity()
+    #plot_historgram()
+    scatter_with_plotting_symbol()
