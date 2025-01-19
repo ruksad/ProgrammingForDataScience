57fb73808de35100ab1ba4674ca48050d3285cb6
ruksad <sidsid2810@gmail.com>
2025-01-19 11:45:56 +0530

    text cleaning

--- 13_working_with_text.py
+++ 13_working_with_text.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 import os
 import re
-from timeit_util import time_decorator
 import pandas as pd
+
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def load_data_frame(path: Path):
@@ -16,16 +22,15 @@ def extract_parts(speech):
     return [name, date, body]
 
 
-"""
-r'\[[^\]]+\]'   this will match [laughter] [claps] \[ \] matches the bracket in speech [^\]]+ will match one or more chars which are not 
-"""
+# r'\[[^\]]+\]'   this will match [laughter] [claps] \[ \] matches the bracket in speech [^\]]+ will match one or more chars which are not
 
 
 def clean_text(df):
     brackets_re = re.compile(r'\[[^\]]+\]')
-    apart_from_words= re.compile(r'[^a-z\s]')
+    apart_from_words = re.compile(r'[^a-z\s]')
 
-    cleaned= df['text'].str.lower().str.replace(brackets_re,'',regex=True).str(apart_from_words,' ',regex=True)
+    cleaned = df['text'].str.lower().str.replace(brackets_re, '', regex=True).str.replace(apart_from_words, ' ',
+                                                                                          regex=True)
     return df.assign(text=cleaned)
 
 
@@ -34,11 +39,26 @@ def read_speeches_into_df():
                         columns=["name", "date", "text"])
 
 
+def stemming_tokenizer(document):
+    return [
+        porter_stemmer.stem(word)
+        for word in nltk.word_tokenize(document)
+        if word not in stop_words
+    ]
+
+
 if __name__ == "__main__":
     line = "{}{:<35}".format
     text = load_data_frame(Path('sources'))
     allSpeeches = re.findall(r'\*\*\*', text)
     print(line('Total speeches: ', len(allSpeeches)))
     total_records = text.split('***')
-    df = read_speeches_into_df()
-    df_cleaned = clean_text(df)
+    cleaned_text = read_speeches_into_df().pipe(clean_text)
+    cleaned_text.info()
+    print("\n",cleaned_text.loc[:,["name","date"]])
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    porter_stemmer = PorterStemmer()
+
+    tfidf = TfidfVectorizer(tokenizer=stemming_tokenizer)
+    speech_vectors= tfidf.fit_transform(cleaned_text['text'])
+    print("speech vector shape:",speech_vectors.shape)
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 pip install -r requirements.txt
 
+pip show nltk # to check version of nltk
+
 These collections of programs are taken from the book learning datascience By Sam Lau, Joey Gonzalez, and Deb Nolan. Here we are going to explore how to use numpy and pandas for learning datascience.
